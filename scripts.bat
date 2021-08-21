19b6928851fa0737e4edaa082da5915cd6f5986a
Zheng Ting Yue <yuezhengting.dev@gmail.com>
2021-08-21 11:29:45 +0800

    added batch file for automated commands

---
+++ scripts.bat
@@ -0,0 +1,19 @@
+@echo off
+
+rem changing directory to access main body of Go code.
+cd service
+
+rem building Go code. Checking for errors with if statement.
+echo Building Go Files...
+go build
+if errorlevel 1 echo Build Unsuccessful. && cd .. && exit /b
+
+rem Building successful. This is where the executable created by the build is called.
+echo Build Successful. Starting executable.
+start /wait service.exe
+
+rem The following commands are called when the executable terminates.
+rem Deleting the executable file for clean up purposes.
+echo Service stopped.
+del service.exe
+cd ..
