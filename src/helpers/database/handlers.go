cd2a2ccf2f1ef0d586a5a50e5e1a60dd53b33fe0
Zheng Ting Yue <yuezhengting.dev@gmail.com>
2021-08-29 12:28:30 +0800

    Auto migration

--- src/helpers/database/handlers.go
+++ src/helpers/database/handlers.go
@@ -1,19 +1,28 @@
 package database
 
 import (
-	"fmt"
+	"log"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+
+	"github.com/ztdevelops/go-project/src/helpers/custom_types"
 )
 
 func (database *Database) InitDatabaseConnection() {
-	fmt.Println("Establishing connection with database.")
+	log.Println("Establishing connection with database.")
 
 	dsn := "root:password@tcp(127.0.0.1:3306)/test"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err.Error())
 	}
+	log.Println("Database connection OK on port 127.0.0.1:3306")
 	database.DB = db
+	database.MigrateTables()
+}
+
+func (database *Database) MigrateTables() {
+	log.Println("Migrating tables.")
+	database.AutoMigrate(&custom_types.User{})
 }
