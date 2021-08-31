95087a7f7ba6687bc00596e493c574586cc882f8
Yue Zheng Ting <yuezhengting@gmail.com>
2021-09-01 01:37:59 +0800

    Added post and comment struct

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.14
 
 require (
 	github.com/gorilla/mux v1.8.0
+	github.com/satori/go.uuid v1.2.0
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gorm.io/driver/mysql v1.1.2
 	gorm.io/gorm v1.21.14
 )
--- src/helpers/custom_types/structs.go
+++ src/helpers/custom_types/structs.go
@@ -1,6 +1,26 @@
 package custom_types
 
+import (
+	uuid "github.com/satori/go.uuid"
+)
+
 type User struct {
+	ID uuid.UUID `json:"id" gorm:"primaryKey"`
 	Username string `json:"username" gorm:"unique;not null"`
 	Password string `json:"password" gorm:"not null"`
+}
+
+type Post struct {
+	ID uuid.UUID `json:"id" gorm:"primaryKey"`
+	Message string `json:"message"`
+	Username string `json:"username" gorm:"foreignKey:User.Username"`
+	Likes int `json:"likes"`
+}
+
+type Comment struct {
+	ID uuid.UUID `json:"id" gorm:"primaryKey"`
+	PostID []byte `json:"post_id" gorm:"foreignKey:Post.ID"`
+	Message string `json:"message"`
+	Username string `json:"username" gorm:"foreignKey:User.Username"`
+	Likes int `json:"likes"`
 }
