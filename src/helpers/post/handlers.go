dad900f705aabaecf6ac18933e2f0992468adbaa
Yue Zheng Ting <yuezhengting@gmail.com>
2021-09-02 15:15:26 +0800

    created functions for CRUD posts & comments (wip)

---
+++ src/helpers/post/handlers.go
@@ -0,0 +1,41 @@
+package post
+
+// CreatePost creates a post record in the database.
+func CreatePost() {
+
+}
+
+// UpdatePost updates an existing post record in the database.
+func UpdatePost() {
+
+}
+
+// DeletePost deletes a specific post record from the database, as well as the existing comments associated with it.
+func DeletePost() {
+
+}
+
+// GetPosts gets all post records from the database. In future, this function will support filtering.
+func GetPosts() {
+
+}
+
+// GetPostByID gets a specific post record from the database.
+func GetPostByID() {
+
+}
+
+// GetCommentsByPostID gets all the comments associated with the selected post record.
+func GetCommentsByPostID() {
+
+}
+
+// UpdateComment updates a specific comment record in the database.
+func UpdateComment() {
+
+}
+
+// DeleteComment deletes a specific comment from the database.
+func DeleteComment() {
+
+}
