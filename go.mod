90ba862e21ac730e1ce32145e44c602e07cc78d2
Yue Zheng Ting <yuezhengting@gmail.com>
2021-09-03 09:58:06 +0800

    jwt middleware

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/ztdevelops/go-project
 go 1.14
 
 require (
+	github.com/auth0/go-jwt-middleware v1.0.1
+	github.com/form3tech-oss/jwt-go v3.2.2+incompatible
 	github.com/gorilla/mux v1.8.0
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5
 	gorm.io/driver/mysql v1.1.2
--- src/helpers/custom_types/structs.go
+++ src/helpers/custom_types/structs.go
@@ -4,3 +4,16 @@ type User struct {
 	Username string `json:"username" gorm:"unique;not null"`
 	Password string `json:"password" gorm:"not null"`
 }
+
+type Jwks struct {
+	Keys []JSONWebKeys `json:"keys"`
+}
+
+type JSONWebKeys struct {
+	Kty string   `json:"kty"`
+	Kid string   `json:"kid"`
+	Use string   `json:"use"`
+	N   string   `json:"n"`
+	E   string   `json:"e"`
+	X5c []string `json:"x5c"`
+}
