b641a99db14f1ecb11351e797b2d9aa50a761ce5
Yue Zheng Ting <yuezhengting@gmail.com>
2021-09-12 11:18:11 +0800

    clean up

--- src/handlers.go
+++ src/handlers.go
@@ -5,20 +5,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	// "os"
-	// "time"
 
-	// jwtmiddleware "github.com/auth0/go-jwt-middleware"
-	// "github.com/form3tech-oss/jwt-go"
 	"github.com/ztdevelops/go-project/src/helpers/custom_types"
-	// "golang.org/x/crypto/bcrypt"
 )
 
 // HandleRoutes initialises the connections to all the explicitly coded routes.
 func (a *App) HandleRoutes() {
 	app, err := InitFirebase()
 	if err != nil {
-		log.Fatalf("failed to init firebase:", err)
+		log.Fatal("failed to init firebase:", err)
 	}
 
 	a.App = *app
@@ -31,7 +26,6 @@ func (a *App) HandleRoutes() {
 
 	// APIs (Require auth)
 	a.Router.HandleFunc("/api/test", a.TestVerifyToken).Methods(custom_types.RGET)
-	// a.Router.Handle("/api/test", handleWithJWT(jwt, NotImplemented)).Methods(custom_types.RGET)
 	http.Handle("/", a.Router)
 }
 
@@ -42,22 +36,6 @@ func (a *App) TestVerifyToken(w http.ResponseWriter, r *http.Request) {
 	}
 	log.Println("token ok.")
 }
-
-// func handleWithJWT(middleware *jwtmiddleware.JWTMiddleware, f func(http.ResponseWriter, *http.Request)) http.Handler {
-// 	httpHandler := http.HandlerFunc(f)
-// 	return middleware.Handler(httpHandler)
-// }
-
-// func generateToken(userID int) (token string, err error) {
-// 	os.Setenv("ACCESS_SECRET", "secret")
-// 	atClaims := jwt.MapClaims{}
-// 	atClaims["authorized"] = true
-// 	atClaims["user_id"] = userID
-// 	atClaims["exp"] = time.Now().Add(time.Minute * 10).Unix()
-// 	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
-// 	token, err = at.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
-// 	return
-// }
 
 // Respond responds to the API requests with a status code and message.
 func (w *Writer) Respond(code int, message interface{}) {
@@ -83,8 +61,6 @@ func DefaultHandler(w http.ResponseWriter, r *http.Request) {
 func TestAPIHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println(custom_types.ENDPOINT_HIT, "test")
 	users := []custom_types.User{
-		// {Username: "user 1", Password: "123"},
-		// {Username: "user 2", Password: "456"},
 	}
 	json.NewEncoder(w).Encode(users)
 }
@@ -92,81 +68,3 @@ func TestAPIHandler(w http.ResponseWriter, r *http.Request) {
 func NotImplemented(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Not implemented.")
 }
-
-// HandleSignUp creates an account based on the submitted request.
-// func HandleSignUp(w http.ResponseWriter, r *http.Request) {
-// 	log.Println(custom_types.ENDPOINT_HIT, "sign up")
-// 	writer := Writer{w}
-// 	writer.SetContentType(custom_types.ContentTypeJSON)
-
-// 	received := &custom_types.User{}
-// 	err := json.NewDecoder(r.Body).Decode(received)
-// 	if err != nil {
-// 		log.Println("Cannot decode:", err)
-// 		writer.Respond(http.StatusBadRequest, err.Error())
-// 		return
-// 	}
-
-// 	if received.Username == "" || received.Password == "" {
-// 		log.Println("Received empty credentials. Rejecting request.")
-// 		writer.Respond(http.StatusBadRequest, "Failed to create account: invalid credentials")
-// 		return
-// 	}
-
-// 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(received.Password), 8)
-// 	if err != nil {
-// 		log.Println("Error hashing password:", err)
-// 		writer.Respond(http.StatusBadRequest, err.Error())
-// 		return
-// 	}
-
-// 	submitted := &custom_types.User{
-// 		Username: received.Username,
-// 		Password: string(hashedPassword),
-// 	}
-
-// 	if err := SharedApp.SignUp(*submitted); err != nil {
-// 		log.Println("Cannot sign up:", err)
-// 		writer.Respond(http.StatusBadRequest, err.Error())
-// 		return
-// 	}
-
-// 	writer.Respond(http.StatusOK, "User successfully created.")
-// }
-
-// HandleSignIn attempts to log the user into their accounts based on the submitted credentials.
-// func HandleSignIn(w http.ResponseWriter, r *http.Request) {
-// 	log.Println(r.RemoteAddr, custom_types.ENDPOINT_HIT, "sign in")
-// 	writer := Writer{w}
-// 	writer.SetContentType(custom_types.ContentTypeJSON)
-	
-// 	received := &custom_types.User{}
-// 	if err := json.NewDecoder(r.Body).Decode(received); err != nil {
-// 		writer.Respond(http.StatusBadRequest, err.Error())
-// 		return
-// 	}
-// 	user, err := SharedApp.GetUser(received.Username)
-// 	if err != nil {
-// 		log.Println("Cannot get user:", err)
-// 		writer.Respond(http.StatusUnauthorized, "Credentials are incorrect")
-// 		return
-// 	}
-// 	if err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(received.Password)); err != nil {
-// 		writer.Respond(http.StatusUnauthorized, "Credentials are incorrect")
-// 		return
-// 	}
-
-// 	token, err := generateToken(user.ID)
-// 	if err != nil {
-// 		writer.Respond(http.StatusInternalServerError, "Unable to generate token")
-// 		return
-// 	}
-
-// 	writer.Respond(http.StatusOK, struct{
-// 		Message string
-// 		JWT string
-// 	} {
-// 		"Login OK.",
-// 		token,
-// 	})
-// }
--- src/middleware.go
+++ src/middleware.go
@@ -28,7 +28,7 @@ func GetCorsWrapper(allowedHeaders, allowedMethods []string) *cors.Cors {
 func getEnv(key string) string {
 	err := godotenv.Load("../.env")
 	if err != nil {
-		log.Fatalf("error loading .env:", err)
+		log.Println("error loading .env:", err)
 	}
 	log.Println(key, os.Getenv(key))
 	return os.Getenv(key)
@@ -45,13 +45,13 @@ func VerifyToken(a *firebase.App, r *http.Request) (err error) {
 	
 	client, err := a.Auth(ctx)
 	if err != nil {
-		log.Fatalf("error getting auth client:", err)
+		log.Println("error getting auth client:", err)
 	}
 	idToken := strings.Split(bearerToken, "Bearer ")[1]
 
 	_, err = client.VerifyIDToken(ctx, idToken)
 	if err != nil {
-		log.Fatalf("wrong token")
+		log.Println("wrong token")
 	}
 	return
 }
@@ -61,7 +61,7 @@ func LogInWithFirebase(w http.ResponseWriter, r *http.Request) {
 	received := custom_types.User{}
 	err := json.NewDecoder(r.Body).Decode(&received)
 	if err != nil {
-		log.Fatalf("failed to decode user:", err)
+		log.Println("failed to decode user:", err)
 	}
 	log.Println(received)
 	url := fmt.Sprintf("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=%s", getEnv("API_KEY"))
@@ -70,22 +70,22 @@ func LogInWithFirebase(w http.ResponseWriter, r *http.Request) {
 	}
 	u, err := json.Marshal(received)
 	if err != nil {
-		log.Fatalf("error marshalling user:", err)
+		log.Println("error marshalling user:", err)
 	}
 	response, err := http.Post(url, "application/json", bytes.NewBuffer(u))
 	if err != nil {
-		log.Fatalf("error querying firebase api:", err)
+		log.Println("error querying firebase api:", err)
 	}
 
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		log.Fatalf("error reading body:", err)
+		log.Println("error reading body:", err)
 	}
 
 	var result custom_types.UserReponse
 	err = json.Unmarshal(body, &result)
 	if err != nil {
-		log.Fatalf("error unmarshalling response:", err)
+		log.Println("error unmarshalling response:", err)
 	}
 
 	json.NewEncoder(w).Encode(result)
@@ -95,89 +95,3 @@ func LogInWithFirebase(w http.ResponseWriter, r *http.Request) {
 	// 	return
 	// }
 }
-
-// package main
-
-// import (
-// 	"encoding/json"
-// 	"errors"
-// 	"log"
-// 	"net/http"
-
-// 	"github.com/auth0/go-jwt-middleware"
-// 	"github.com/form3tech-oss/jwt-go"
-// 	"github.com/rs/cors"
-// 	"github.com/ztdevelops/go-project/src/helpers/custom_types"
-// )
-
-// func GetCorsWrapper(allowedHeaders, allowedMethods []string) *cors.Cors {
-// 	return cors.New(cors.Options{
-// 		AllowedMethods: allowedMethods,
-// 		AllowedHeaders: allowedHeaders,
-// 	})
-// }
-
-// func RetrieveToken() {
-
-// }
-
-// func GetJWTMiddleware() *jwtmiddleware.JWTMiddleware {
-// 	validationKeyGetter := func(token *jwt.Token) (interface{}, error) {
-// 		// audience := "localhost:8000/api"
-// 		// checkedAudience := token.Claims.(jwt.MapClaims).VerifyAudience(audience, false)
-// 		// if !checkedAudience {
-// 		// 	return token, errors.New("invalid audience")
-// 		// }
-
-// 		issuer := "https://dev-3tmog62q.us.auth0.com/"
-// 		checkIssuer := token.Claims.(jwt.MapClaims).VerifyIssuer(issuer, false)
-// 		if !checkIssuer {
-// 			return token, errors.New("invalid issuer")
-// 		}
-// 		cert, err := getPemCert(token)
-// 		if err != nil {
-// 			panic(err.Error())
-// 		}
-// 		result, _ := jwt.ParseRSAPublicKeyFromPEM([]byte(cert))
-// 		return result, nil
-// 	}
-
-// 	return jwtmiddleware.New(jwtmiddleware.Options{
-// 		ValidationKeyGetter: validationKeyGetter,
-// 		SigningMethod: jwt.SigningMethodHS256,
-// 	})
-// }
-
-// func getPemCert(token *jwt.Token) (string, error) {
-// 	cert := ""
-// 	resp, err := http.Get("https://dev-3tmog62q.us.auth0.com/.well-known/jwks.json")
-
-// 	if err != nil {
-// 		return cert, err
-// 	}
-// 	defer resp.Body.Close()
-
-// 	var jwks = custom_types.Jwks{}
-// 	err = json.NewDecoder(resp.Body).Decode(&jwks)
-
-// 	if err != nil {
-// 		return cert, err
-// 	}
-
-// 	for k, v := range token.Header {
-// 		log.Println("key:", k, "val:", v)
-// 	}
-
-// 	for k, _ := range jwks.Keys {
-// 		if token.Header["kid"] == jwks.Keys[k].Kid {
-// 			cert = "-----BEGIN CERTIFICATE-----\n" + jwks.Keys[k].X5c[0] + "\n-----END CERTIFICATE-----"
-// 		}
-// 	}
-
-// 	if cert == "" {
-// 		err := errors.New("unable to find appropriate key")
-// 		return cert, err
-// 	}
-
-// 	return cert, nil
-// }
